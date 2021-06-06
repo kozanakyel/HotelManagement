a4ed0d23704f8fe8fd0c4755d8b38d7d6a1595a2
kozanakyel <kozanakyel@gmail.com>
2021-06-06 15:46:09 +0300

    Last Cahnges for Client and Admin panel

--- index.php
+++ index.php
@@ -2,8 +2,24 @@
 require 'header/header.php';
 ?>
 <div class="container">
-  <a href="homePage.php" role="button" class="btn btn-success">Home/Customer</a>
-</div>
-<div class="container">
-  <a href="management/loginmng.php" role="button" class="btn btn-danger">Admin login</a>
+  <div class="row">
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Home Pages For Client</h5>
+          <p class="card-text">Registered Client or potential client can go to our project...</p>
+          <a href="homePage.php" role="button" class="btn btn-success">Home/Client</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Management Part For Staff</h5>
+          <p class="card-text">Any staff information and Reservation feedback Procedure</p>
+          <a href="management/loginmng.php" role="button" class="btn btn-danger">Admin login</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
