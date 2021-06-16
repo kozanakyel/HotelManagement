7d814c167bb1d9e78e0a68ab24a5ba24634f7f90
kozanakyel <kozanakyel@gmail.com>
2021-06-17 01:03:09 +0300

    reports page has been made for heroku

--- setting/connection.php
+++ setting/connection.php
@@ -2,8 +2,8 @@
 
 
 try{
-  //$conn = new PDO("mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_336069692980e83;charset=utf8mb4","b58f0f3a8583cb","1ec4cdaa");
-  $conn = new PDO("mysql:host=localhost;dbname=hotelsite;charset=utf8mb4","root","");
+  $conn = new PDO("mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_336069692980e83;charset=utf8mb4","b58f0f3a8583cb","1ec4cdaa");
+  //$conn = new PDO("mysql:host=localhost;dbname=hotelsite;charset=utf8mb4","root","");
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e){
   echo $e->getMessage();
